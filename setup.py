b675110d3201eb33230531b74cf6c857a2d5d52c
Sahana K N <sahana86gowda@gmail.com>
2025-07-19 16:45:55 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+
+
+setup(
+name='mlproject',    
+version = '0.0.1',
+author = "sahana",
+author_email='sahana86gowda@gmail.com',
+packages = find_packages(),
+install_requires = ['pandas','numpy']           
+
+)
